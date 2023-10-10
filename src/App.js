c043c6a5db9c4d6482e1fcd60a1d0b83b77ccbd6
Aliyu <biggy0023@gmail.com>
2023-10-10 11:53:22 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="custom-container bg-secondary">
+      <nav className="navbar navbar-expand-lg nav-color">
+        <div class="container-fluid">
+          <a className="runnr-brand no-deco text-color" href="/">.RUNNR</a> {/* runnnr-brand is something I might have to look into */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link text-color active" aria-current="page" href="home">Home</a>
+              <a className="nav-link text-color" href="marketplace">Marketplace</a>
+              <a className="nav-link text-color" href="financials">Financials</a>
+              <a className="nav-link text-color" href="bookmarks">Bookmarks</a>
+            </div>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-light" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div className="row main-section">
+        <div className="col-3 side-area">hello</div>
+        <div className="col-6 main-main">hi</div>
+      </div>
     </div>
   );
 }
